bd90cbeb82b94dd3a054ffecb458b6d08e6432cf
@douglasabnovato <douglasabnovato@gmail.com>
2019-06-14 16:08:01 -0300

    upload image

--- src/config/upload.js
+++ src/config/upload.js
@@ -4,6 +4,9 @@ const path = require('path');
 module.exports = {
     storage: new multer.diskStorage({
         destination: path.resolve(__dirname, '..', '..', 'uploads'),
+        filename: function(req, file, cb){
+            cb(null, file.originalname);
+        }
     })
 };
 
