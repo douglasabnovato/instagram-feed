2079752a0877d8b6c956c82d5f8075d2a25aab9d
@douglasabnovato <douglasabnovato@gmail.com>
2019-12-20 17:44:11 -0300

    mongoose

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+mongoose.connect('mongodb+srv://omnistack7:omnistack7@cluster0-fygh8.mongodb.net/test?retryWrites=true&w=majority',{
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: true
+});
 
 app.get('/', (req, res) => {
-    return res.send(`Hi ${req.query.name}.`);
+    return res.send(`Hello ${req.query.name}.`);
 });
 
 app.listen(3333);
