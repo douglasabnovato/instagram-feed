a09d17ca4616e730e31d614d25bcb4d750e20e1a
@douglasabnovato <douglasabnovato@gmail.com>
2019-12-21 16:59:25 -0300

    deletar imagem original

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -19,7 +19,9 @@ module.exports = {
             .toFile(
                 path.resolve(req.file.destination, 'resized', image)
             )
-
+        
+        fs.unlinkSync(req.file.path);
+            
         const post = await Post.create({
             author,
             place,
