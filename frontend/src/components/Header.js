7defffdf30e27823f766ccb234bb3cfe1c73368e
@douglasabnovato <douglasabnovato@gmail.com>
2020-01-02 15:07:55 -0300

    rota e link do feed e do new

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './Header.css';
 
@@ -9,8 +10,12 @@ export default function Header(){
     return(
         <header id="main-header">
             <div className="header-content">
-                <img src={ logo } alt="InstaRocket" />
-                <img src={ camera } alt="Enviar publicação" />
+                <Link to="/">
+                    <img src={ logo } alt="InstaRocket" />
+                </Link>
+                <Link to="/new">
+                    <img src={ camera } alt="Enviar publicação" />
+                </Link>                
             </div>
         </header>
     );
