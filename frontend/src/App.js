921dd5c38fafeb10d973c8c7f94412007bdbdc0d
@douglasabnovato <douglasabnovato@gmail.com>
2020-01-02 14:06:13 -0300

    header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter } from 'react-router-dom';
 import './global.css';
 
 import Routes from './routes';
-import { Route } from 'react-router-dom';
 
 function App() {
   return (
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Header from './Header.css'
+
+export default function Header(){
+    render(){
+        return(
+            <header id="main-header">
+                <div className="header-content">
+                    <img src="" alt="InstaRocket" />
+                    <img src="" alt="Enviar publicação" />
+                </div>
+            </header>
+            
+        );
+    }
+}
