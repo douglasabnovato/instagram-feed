7952a5e2f0d3e52c09b2675b14ac8d1d219e126d
@douglasabnovato <douglasabnovato@gmail.com>
2020-01-03 18:41:19 -0300

    likedpost

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -28,6 +28,14 @@ class Feed extends Component{
         socket.on('post', newPost => {
             this.setState({ feed: [ newPost, ...this.state.feed] });
         })
+
+        socket.on('like', likedPost => {
+            this.setState({
+                feed: this.state.feed.map(post => 
+                    post.id === likedPost.id ? likedPost : post
+                )
+            });                
+        })
     }
 
     handleLike = id => {
