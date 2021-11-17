037d7526bade49916d3668d8a6e0a295a9618568
Douglas A B Novato <douglasabnovato@gmail.com>
2021-11-17 12:49:40 -0300

    estrutura básica de react native

---
+++ instarocket/src/pages/New.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+
+export default class New extends Component {
+    render() {
+        return (
+            <View/>
+        )
+    }
+}
