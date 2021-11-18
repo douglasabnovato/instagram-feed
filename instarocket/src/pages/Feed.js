2e30bdf99e38aadaf6673e2f3983bd2799438088
Douglas A B Novato <douglasabnovato@gmail.com>
2021-11-18 11:02:28 -0300

    organizando as tarefas de hoje

--- instarocket/src/pages/Feed.js
+++ instarocket/src/pages/Feed.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import { View, Image, TouchableOpacity } from 'react-native';
+
+import camera from "../assets/camera.svg";
 
 export default class Feed extends Component {
+    
+    static navigationOptions = {
+        headerRight: (
+            <TouchableOpacity style={{ marginRight: 20 }} onPress={() => {}} >
+                <Image source={camera}/>
+            </TouchableOpacity>
+        ),
+    };
+    
     render() {
         return (
             <View/>
         )
     }
+
 }
--- instarocket/src/routes.js
+++ instarocket/src/routes.js
@@ -1,11 +1,24 @@
+import React from "react";
 import { createAppContainer, createStackNavigator } from "react-navigation";
+import { Image } from "react-native";
 
 import Feed from "./pages/Feed";
 import New from "./pages/New";
 
-export default createAppConteiner(
-    createStackNavigatior({
+import logo from "./assets/instagram.png";
+
+export default createAppContainer(
+    createStackNavigator({
         Feed,
         New,
+    }, {
+        defaultNavigationOptions: {
+            headerTintColor: "#000",
+            headerTitle: <Image style={{ marginHorizontal: 20 }} source={ logo }/>,
+            headerBackTitle: null,
+        },
+        mode: "modal",
     })
 );
+
+
