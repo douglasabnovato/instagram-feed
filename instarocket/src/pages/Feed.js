895fa7990454a3a7159ace842e56a67c8201de27
Douglas A B Novato <douglasabnovato@gmail.com>
2021-11-18 15:23:18 -0300

    icons and container

--- instarocket/src/pages/Feed.js
+++ instarocket/src/pages/Feed.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'
-import { View, Image, TouchableOpacity } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
 import api from '../services/api';
 
-import camera from "../assets/camera.svg";
+import camera from "../assets/camera.png";
+import more from "../assets/more.png";
+import like from "../assets/like.png";
+import comment from "../assets/comment.png";
+import send from "../assets/send.png"; 
 
 export default class Feed extends Component {
     
@@ -26,8 +30,36 @@ export default class Feed extends Component {
     
     render() {
         return (
-            <View/>
+            <View style={styles.container}>
+                
+                <FlatList 
+                    data={ this.state.feed }
+                    keyExtractor={ post => post._id }
+                    renderItem={({ item }) => (
+
+                        <View style={styles.feedItem}>
+
+                            <View style={styles.feedItemHeader}>
+
+                                <View style={styles.userInfo}>
+                                    <Text style={ styles.name }>{item.author}</Text>
+                                    <Text style={ styles.place }>{item.place}</Text>
+                                </View>
+
+                                <Image source={more}/>
+                            </View> 
+
+                            <Image style={styles.feedImage} source={{ url: `http://localhost:3333/${item.image}` }}/>
+ 
+                        </View>
+ 
+                    )}
+                />
+
+            </View>
         )
-    }
+    } 
 
 }
+
+const styles = StyleSheet.create({});
